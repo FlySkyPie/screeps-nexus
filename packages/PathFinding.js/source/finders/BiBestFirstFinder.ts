0a4142fcf11a3b9ccc1b698750983871fa7fe50f
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-12-08 16:33:35 +0800

    refactor(pathfinding): Add Typescript

---
+++ packages/PathFinding.js/source/finders/BiBestFirstFinder.ts
@@ -0,0 +1,25 @@
+import BiAStarFinder from './BiAStarFinder';
+
+/**
+ * Bi-direcitional Best-First-Search path-finder.
+ * @constructor
+ * @extends BiAStarFinder
+ * @param {object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed. Deprecated, use diagonalMovement instead.
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching block corners. Deprecated, use diagonalMovement instead.
+ * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ */
+class BiBestFirstFinder extends BiAStarFinder {
+    constructor(opt) {
+        super(opt);
+
+        const orig = this.heuristic;
+        this.heuristic = (dx, dy) => {
+            return orig(dx, dy) * 1000000;
+        };
+    }
+}
+
+export default BiBestFirstFinder;
---
+++ packages/PathFinding.js/source/finders/JumpPointFinderBase.ts
@@ -0,0 +1,139 @@
+/**
+ * @author imor / https://github.com/imor
+ */
+import Heap from 'heap';
+
+import Util from '../core/Util';
+import Heuristic from '../core/Heuristic';
+import DiagonalMovement from '../core/DiagonalMovement';
+
+/**
+ * Base class for the Jump Point Search algorithm
+ * @param {object} opt
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ */
+class JumpPointFinderBase {
+	public heuristic: any;
+	public trackJumpRecursion: any;
+	public openList: any;
+	public startNode: any;
+	public endNode: any;
+	public grid: any;
+	public _findNeighbors: any;
+	public _jump: any;
+
+    constructor(opt) {
+        opt = opt || {};
+        this.heuristic = opt.heuristic || Heuristic.manhattan;
+        this.trackJumpRecursion = opt.trackJumpRecursion || false;
+    }
+
+    /**
+     * Find and return the path.
+     * @return {Array.<[number, number]>} The path, including both start and
+     *     end positions.
+     */
+    findPath(startX, startY, endX, endY, grid) {
+        const openList = this.openList = new Heap((nodeA, nodeB) => {
+                return nodeA.f - nodeB.f;
+            });
+
+        const startNode = this.startNode = grid.getNodeAt(startX, startY);
+        const endNode = this.endNode = grid.getNodeAt(endX, endY);
+        let node;
+
+        this.grid = grid;
+
+
+        // set the `g` and `f` value of the start node to be 0
+        startNode.g = 0;
+        startNode.f = 0;
+
+        // push the start node into the open list
+        openList.push(startNode);
+        startNode.opened = true;
+
+        // while the open list is not empty
+        while (!openList.empty()) {
+            // pop the position of node which has the minimum `f` value.
+            node = openList.pop();
+            node.closed = true;
+
+            if (node === endNode) {
+                return Util.expandPath(Util.backtrace(endNode));
+            }
+
+            this._identifySuccessors(node);
+        }
+
+        // fail to find the path
+        return [];
+    }
+
+    /**
+     * Identify successors for the given node. Runs a jump point search in the
+     * direction of each available neighbor, adding any points found to the open
+     * list.
+     * @protected
+     */
+    _identifySuccessors(node) {
+        const grid = this.grid;
+        const heuristic = this.heuristic;
+        const openList = this.openList;
+        const endX = this.endNode.x;
+        const endY = this.endNode.y;
+        let neighbors;
+        let neighbor;
+        let jumpPoint;
+        let i;
+        let l;
+        const x = node.x;
+        const y = node.y;
+        let jx;
+        let jy;
+        let dx;
+        let dy;
+        let d;
+        let ng;
+        let jumpNode;
+        const abs = Math.abs;
+        const max = Math.max;
+
+        neighbors = this._findNeighbors(node);
+        for(i = 0, l = neighbors.length; i < l; ++i) {
+            neighbor = neighbors[i];
+            jumpPoint = this._jump(neighbor[0], neighbor[1], x, y);
+            if (jumpPoint) {
+
+                jx = jumpPoint[0];
+                jy = jumpPoint[1];
+                jumpNode = grid.getNodeAt(jx, jy);
+
+                if (jumpNode.closed) {
+                    continue;
+                }
+
+                // include distance, as parent may not be immediately adjacent:
+                d = Heuristic.octile(abs(jx - x), abs(jy - y));
+                ng = node.g + d; // next `g` value
+
+                if (!jumpNode.opened || ng < jumpNode.g) {
+                    jumpNode.g = ng;
+                    jumpNode.h = jumpNode.h || heuristic(abs(jx - endX), abs(jy - endY));
+                    jumpNode.f = jumpNode.g + jumpNode.h;
+                    jumpNode.parent = node;
+
+                    if (!jumpNode.opened) {
+                        openList.push(jumpNode);
+                        jumpNode.opened = true;
+                    } else {
+                        openList.updateItem(jumpNode);
+                    }
+                }
+            }
+        }
+    }
+}
+
+export default JumpPointFinderBase;
